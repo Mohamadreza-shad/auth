083db834f4e0036c467bae167e127993f9fbbd63
Mohamadreza Shad <mohamadreza.shad@wallit.eu>
2025-11-28 16:26:58 +0330

    refactor config

--- config/config.go
+++ config/config.go
@@ -12,30 +12,19 @@ type Config struct {
 }
 
 type I18n struct {
-	ResourcePath string `required:"true" envconfig:"GO_HEXAGONAL_I18N_RESOURCE_PATH"`
-	Languages    string `required:"true" envconfig:"GO_HEXAGONAL_I18N_LANGUAGES"`
-}
-
-type Graylog struct {
-	Format         string `required:"true" envconfig:"GO_HEXAGONAL_GRAYLOG_FORMAT"`
-	Level          string `required:"true" envconfig:"GO_HEXAGONAL_GRAYLOG_LEVEL"`
-	NoColor        bool   `required:"true" envconfig:"GO_HEXAGONAL_GRAYLOG_NOCOLOR"`
-	GraylogAddress string `required:"true" envconfig:"GO_HEXAGONAL_GRAYLOG_ADDRESS"`
-	ServiceName    string `required:"true" envconfig:"GO_HEXAGONAL_GRAYLOG_SERVICE_NAME"`
-	Version        string `required:"true" envconfig:"GO_HEXAGONAL_GRAYLOG_VERSION"`
-	Facility       string `required:"true" envconfig:"GO_HEXAGONAL_GRAYLOG_FACILITY"`
+	ResourcePath string `required:"true" envconfig:"I18N_RESOURCE_PATH"`
+	Languages    string `required:"true" envconfig:"I18N_LANGUAGES"`
 }
 
 type Database struct {
-	DSN          string `required:"true" envconfig:"GO_HEXAGONAL_DATABASE_DNS"`
-	MaxOpenConns int    `required:"true" envconfig:"GO_HEXAGONAL_DATABASE_MAX_OPEN_CONNECTION"`
-	MaxIdleConns int    `required:"true" envconfig:"GO_HEXAGONAL_DATABASE_MAX_IDLE_CONNECTION"`
+	DSN          string `required:"true" envconfig:"DATABASE_DNS"`
+	MaxOpenConns int    `required:"true" envconfig:"DATABASE_MAX_OPEN_CONNECTION"`
+	MaxIdleConns int    `required:"true" envconfig:"DATABASE_MAX_IDLE_CONNECTION"`
 }
 
 type Server struct {
-	HttpAddress   string `required:"true" envconfig:"GO_HEXAGONAL_SERVER_HTTP_ADDRESS"`
-	GrpcAddress   string `required:"true" envconfig:"GO_HEXAGONAL_SERVER_GRPC_ADDRESS"`
-	HealthAddress string `required:"true" envconfig:"GO_HEXAGONAL_SERVER_HEALTH_ADDRESS"`
+	HttpAddress   string `required:"true" envconfig:"SERVER_HTTP_ADDRESS"`
+	HealthAddress string `required:"true" envconfig:"SERVER_HEALTH_ADDRESS"`
 }
 
 func GetConfig() (Config, error) {
